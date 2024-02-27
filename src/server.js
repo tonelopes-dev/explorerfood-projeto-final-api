ef9190749eb0c130a0976c09753e626197acb274
Tone Lopes <tonelopes.dev@gmail.com>
2024-02-27 09:24:20 -0300

    fix: link deploy

--- src/server.js
+++ src/server.js
@@ -5,13 +5,17 @@ const uploadConfig = require("./configs/upload");
 const routes = require("./routes");
 const cookieParser = require("cookie-parser");
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://explorerfood-projeto-final-web.vercel.app"],
+  credentials: true, // Permitir cookies e autenticação de sessão
+  // Adicione outras opções conforme necessário
+};
+
 const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://explorerfood-projeto-final-web.vercel.app/"], credentials: true })
-);
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 const PORT = process.env.PORT || 3333;
