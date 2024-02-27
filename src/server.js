7363325b05453961dc43bc487d3a9a3f647441c3
Tone Lopes <tonelopes.dev@gmail.com>
2024-02-27 08:58:20 -0300

    fix: add link render deploy.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173/"], credentials: true }));
+app.use(
+  cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://explorerfood-projeto-final-web.vercel.app/"], credentials: true })
+);
 
 app.use(cookieParser());
 const PORT = process.env.PORT || 3333;
