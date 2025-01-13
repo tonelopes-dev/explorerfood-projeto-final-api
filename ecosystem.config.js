83035c8013c4b7081ec844f066390cac708ea096
Tone Lopes Monte <tonelopes.dev@gmail.com>
2025-01-13 19:09:40 -0300

    Update ecosystem.config.js to 1 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "api",
       script: "./src/server.js",
-      instances: "max",
+      instances: 1,
       exec_mode: "cluster",
       watch: false,
       increment_var: "PORT",
